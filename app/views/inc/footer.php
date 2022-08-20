4e0db66525334dc2e04b1ea1a2c63db10463c807
Jeramiah Coffey <jcoffey@rategenius.com>
2022-08-20 13:35:02 -0500

    Bootstrap and FontAwesome

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,4 +1,6 @@
-  <script src="<?php echo URL_ROOT; ?>/js/main.js"></script>
-  </body>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+<script src="<?php echo URL_ROOT; ?>/js/main.js"></script>
+</body>
 
-  </html>
+</html>
