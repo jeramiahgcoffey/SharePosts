1265135f1a194933268cc6335eae140dacebd63c
Jeramiah Coffey <jcoffey@rategenius.com>
2022-08-20 22:39:56 -0500

    git add . Posts controller

---
+++ app/views/posts/index.php
@@ -0,0 +1,12 @@
+<?php require APP_ROOT . '/views/inc/header.php'; ?>
+<div class="row align-items-center">
+  <div class="col-6">
+    <h1>Posts</h1>
+  </div>
+  <div class="col-6">
+    <a href="<?php echo URL_ROOT; ?>/posts/add" class="btn btn-primary float-right">
+      <i class="fa fa-pencil"></i> Add Post
+    </a>
+  </div>
+</div>
+<?php require APP_ROOT . '/views/inc/footer.php';
